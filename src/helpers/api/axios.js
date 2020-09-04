a29de2a0e577e796297914435239e5210f4334f7
dickymahfudin <dickymahfudin13@gmail.com>
2020-09-04 13:58:06 +0700

    axios baseUrl apt1

--- src/helpers/api/axios.js
+++ src/helpers/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const baseURLApt1 = "http://127.0.0.1:8000";
-const access_token =
+const accessToken =
   "860Y2paeQbjXa0hQo2sV2JVwftxAKZLpDGZUxEpFTk9kvcyYYTvQSMo1R4yL4qN2GV4DJ6K5WjgrSmAP";
 
 export const instanceApt1 = axios.create({
   baseURL: baseURLApt1,
   headers: {
-    Authorization: `Bearer ${access_token}`,
+    Authorization: `Bearer ${accessToken}`,
   },
 });
