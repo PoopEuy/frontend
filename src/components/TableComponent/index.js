ac719b12fd8788c424736aca1b3d3af7ab6746ff
dickymahfudin <dickymahfudin13@gmail.com>
2020-09-09 10:56:01 +0700

    add error props

--- src/components/TableComponent/index.js
+++ src/components/TableComponent/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import MUIDataTable from "mui-datatables";
 
-//material ui core
-import { Container } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-//redux
-import { connect } from "react-redux";
-
-const TableComponent = ({ dataTable, title, customToolbar }) => {
+const TableComponent = ({
+  dataTable,
+  title,
+  error,
+  customToolbar,
+  maxTable,
+}) => {
   let options = {
     filter: false,
     sort: true,
-    tableBodyMaxHeight: "550px",
+    tableBodyMaxHeight: maxTable,
     selectableRowsHideCheckboxes: true,
     selectableRowsHeader: false,
     responsive: "standard",
@@ -38,7 +40,7 @@ const TableComponent = ({ dataTable, title, customToolbar }) => {
     customToolbar: customToolbar,
   };
   return (
-    <Container>
+    <>
       {dataTable ? (
         <MUIDataTable
           title={title}
@@ -47,21 +49,36 @@ const TableComponent = ({ dataTable, title, customToolbar }) => {
           options={options}
         />
       ) : (
-        <h1> Error </h1>
+        <div>
+          {error ? (
+            <h1>Network Error</h1>
+          ) : (
+            <CircularProgress color="secondary" />
+          )}
+        </div>
       )}
-    </Container>
+    </>
   );
 };
 
 TableComponent.propTypes = {
   customToolbar: PropTypes.func,
+  maxTable: PropTypes.number,
+  dataTable: PropTypes.any,
+  title: PropTypes.string,
+  error: PropTypes.any,
 };
 
-const mapStateToProps = (state) => {
-  return {
-    dataTable: state.DataTable.dataTable,
-    title: state.DataTable.title,
-  };
+TableComponent.defaultProps = {
+  maxTable: 550,
 };
 
-export default connect(mapStateToProps, null)(TableComponent);
+// const mapStateToProps = (state) => {
+//   return {
+//     dataTable: state.DataTable.dataTable,
+//     title: state.DataTable.title,
+//     error: state.DataTable.error,
+//   };
+// };
+
+export default TableComponent;
