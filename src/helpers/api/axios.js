5adff716d78121d9873cd9b586d35769a682ecf3
dickymahfudin <dickymahfudin13@gmail.com>
2020-10-20 14:33:44 +0700

    read .env file

--- src/helpers/api/axios.js
+++ src/helpers/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const baseURLApt1 = "http://127.0.0.1:8000";
+const baseURLApt1 = process.env.NEXT_PUBLIC_BASE_URL_APT1;
 const accessToken =
   "860Y2paeQbjXa0hQo2sV2JVwftxAKZLpDGZUxEpFTk9kvcyYYTvQSMo1R4yL4qN2GV4DJ6K5WjgrSmAP";
-
 export const instanceApt1 = axios.create({
   baseURL: baseURLApt1,
   headers: {
