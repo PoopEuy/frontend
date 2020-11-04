fde6029cc6f24f58527ba9d095944660bcb7cea5
ramaand <gilangramadhanii191@gmail.com>
2020-11-04 14:03:38 +0700

    fix dataMapOP

--- src/helpers/dataMapOP.js
+++ src/helpers/dataMapOP.js
@@ -6,8 +6,28 @@ function percentage(value, fromLow, fromHigh, toLow, toHigh) {
   return toLow + valueScaled * toSpan;
 }
 
+const getMax = async (data) => {
+  let promise = new Promise((resolve, reject) => {
+    if (data && data.length) {
+      let harvest = { max_avg: 0, length: 0 };
+      let enjoy = { max_avg: 0, length: 0 };
+      data.forEach((el) => {
+        enjoy.max_avg =
+          el.enjoy_total > enjoy.max_avg ? el.enjoy_total : enjoy.max_avg;
+        harvest.max_avg =
+          el.harvest_total > harvest.max_avg
+            ? el.harvest_total
+            : harvest.max_avg;
+      });
+      resolve({ harvest: harvest.max_avg, enjoy: enjoy.max_avg });
+    } else {
+      resolve({ harvest: 0, enjoy: 0 });
+    }
+  });
+  return promise;
+};
+
 export const dataMapOP = async (res, project_name) => {
-  console.log(`dataMapOP ${project_name}`, {res});
   return new Promise(async (resolve, reject) => {
     let max_average;
     let max_percentage = 100;
@@ -25,47 +45,68 @@ export const dataMapOP = async (res, project_name) => {
     const red = "rgba(230, 0, 0, 1)";
     const black = "rgba(0, 0, 0, 1)";
 
+    console.log(`${project_name} is `, res);
     await getMax(res).then((val) => {
-      max_average = {harvest: val.harvest, enjoy: val.enjoy};
+      max_average = { harvest: val.harvest, enjoy: val.enjoy };
+      if (res) {
+        res.forEach((data, index) => {
+          if (data.harvest_total == null) {
+            harvest.push(100);
+            color_harvest.push(black);
+          } else {
+            total_harvest = Math.abs(
+              Math.round(
+                percentage(
+                  data.harvest_total,
+                  0,
+                  max_average.harvest,
+                  0,
+                  max_percentage
+                )
+              )
+            );
+            if (data.harvest_total == 0) total_harvest = 0;
+            if (total_harvest > max_percentage) total_harvest = max_percentage;
+            harvest.push(total_harvest);
+            color_harvest.push(green);
+          }
 
-      res.forEach((data, index) => {
-        if (data.harvest_total == null) {
-          harvest.push(100);
-          color_harvest.push(black);
-        } else {
-          total_harvest = Math.abs(
-            Math.round(
-              percentage(data.harvest_total, 0, max_average.harvest, 0, max_percentage)
-            )
-          );
-          if (data.harvest_total == 0) total_harvest = 0;
-          if (total_harvest > max_percentage) total_harvest = max_percentage;
-          harvest.push(total_harvest);
-          color_harvest.push(green);
-        }
+          if (data.enjoy_total == null) {
+            enjoy.push(100);
+            color_enjoy.push(black);
+          } else {
+            total_enjoy = Math.abs(
+              Math.round(
+                percentage(
+                  data.enjoy_total,
+                  0,
+                  max_average.enjoy,
+                  0,
+                  max_percentage
+                )
+              )
+            );
+            if (data.enjoy_total == 0) total_enjoy = 0;
+            if (total_enjoy > max_percentage) total_enjoy = max_percentage;
+            enjoy.push(total_enjoy);
+            color_enjoy.push(red);
+          }
 
-        if (data.enjoy_total == null) {
-          enjoy.push(100);
-          color_enjoy.push(black);
-        } else {
-          total_enjoy = Math.abs(
-            Math.round(
-              percentage(data.enjoy_total, 0, max_average.enjoy, 0, max_percentage)
-            )
-          );
-          if (data.enjoy_total == 0) total_enjoy = 0;
-          if (total_enjoy > max_percentage) total_enjoy = max_percentage;
-          enjoy.push(total_enjoy);
-          color_enjoy.push(red);
-        }
+          time_stamp.push(data.time_stamp);
+          labels.push(index);
+        });
 
-        time_stamp.push(data.time_stamp);
-        labels.push(index);
-      });
+        if (res.length < 36) {
+          if (res.length == 0) {
+            for (let i = 0; i < 36 - res.length; i++) {
+              time_stamp.push(100);
+              harvest.push(100);
+              enjoy.push(100);
 
-      if (res.length < 36) {
-        if (data.length == 0) {
-          for (let i = 0; i < 36 - data.length; i++) {
+              color_harvest.push(black);
+              color_enjoy.push(black);
+            }
+          } else {
             time_stamp.push(100);
             harvest.push(100);
             enjoy.push(100);
@@ -73,43 +114,44 @@ export const dataMapOP = async (res, project_name) => {
             color_harvest.push(black);
             color_enjoy.push(black);
           }
-        } else {
-          time_stamp.push(100);
-          harvest.push(100);
-          enjoy.push(100);
-
-          color_harvest.push(black);
-          color_enjoy.push(black);
         }
-      }
 
-      chart = {
-        project_name: project_name,
-        time_stamp: time_stamp.reverse(),
-        harvest: harvest.reverse(),
-        enjoy: enjoy.reverse(),
-        color_harvest: color_harvest.reverse(),
-        color_enjoy: color_enjoy.reverse(),
-        labels: labels,
-      };
-      resolve(chart);
+        chart = {
+          project_name: project_name,
+          time_stamp: time_stamp.reverse(),
+          harvest: harvest.reverse(),
+          enjoy: enjoy.reverse(),
+          color_harvest: color_harvest.reverse(),
+          color_enjoy: color_enjoy.reverse(),
+          labels: labels,
+        };
+        resolve(chart);
+      } else {
+        for (let i = 1; i <= 36; i++) {
+          harvest.push(100);
+          color_harvest.push(black);
+          enjoy.push(100);
+          color_enjoy.push(black);
+          time_stamp.push(100);
+          labels.push(i);
+        }
+        chart = {
+          project_name: project_name,
+          time_stamp: time_stamp.reverse(),
+          harvest: harvest.reverse(),
+          enjoy: enjoy.reverse(),
+          color_harvest: color_harvest.reverse(),
+          color_enjoy: color_enjoy.reverse(),
+          labels: labels,
+        };
+        resolve(chart);
+      }
     });
   });
 };
 
-const getMax = async (data) => {
-  let promise = new Promise((resolve, reject) => {
-    let harvest = {max_avg: 0, length: 0};
-    let enjoy = {max_avg: 0, length: 0};
-    data.forEach((el) => {
-      // enjoy.max_avg += el.enjoy_total;
-      // harvest.max_avg += el.harvest_total;
-      enjoy.max_avg = el.enjoy_total > enjoy.max_avg ? el.enjoy_total : enjoy.max_avg;
-      harvest.max_avg = el.harvest_total > harvest.max_avg ? el.harvest_total : harvest.max_avg;
-    });
-    // enjoy.max_avg = enjoy.max_avg / data.length;
-    // harvest.max_avg = harvest.max_avg / data.length;
-    resolve({harvest: harvest.max_avg, enjoy: enjoy.max_avg});
+export const dataMapChint = async (res, serial_number) => {
+  return new Promise(async (resolve, reject) => {
+
   });
-  return promise;
 };
