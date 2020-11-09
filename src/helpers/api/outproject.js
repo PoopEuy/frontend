4fbe94f5b075466931114b19d4f9c607632f4f1a
ramaand <gilangramadhanii191@gmail.com>
2020-11-09 10:07:09 +0700

    +get data Chint and Inverter

--- src/helpers/api/outproject.js
+++ src/helpers/api/outproject.js
@@ -2,6 +2,8 @@ import * as service from "./axios";
 
 const param_get_live_data = "/total?tz=Asia/Jakarta&noc=true&project_name=";
 const param_get_project_name = "/project";
+const param_get_chint = "/chint/5minutes?tz=Asia/Jakarta&noc=true&project_name=";
+const param_get_inverter = "/inverter/5minutes?tz=Asia/Jakarta&noc=true&project_name=";
 
 export const opGetLiveData = async (project_name) => {
   let url = `${param_get_live_data}${project_name}`;
@@ -10,6 +12,7 @@ export const opGetLiveData = async (project_name) => {
     .then((res) => {
       return {
         data: res.data[0].data,
+        node_id: res.data[0].node_id,
         error: false,
       };
     })
@@ -30,6 +33,46 @@ export const opGetProjectName = async () => {
     .then((res) => {
       return {
         data: res.data.projects,
+        error: false,
+      };
+    })
+    .catch((error) => {
+      return {
+        data: false,
+        error: error,
+      };
+    });
+
+  return result;
+};
+
+export const opGetChint = async (project_name) => {
+  let url = `${param_get_chint}${project_name}`;
+  const result = await service.outProjectService
+    .get(url)
+    .then((res) => {
+      return {
+        data: res.data,
+        error: false,
+      };
+    })
+    .catch((error) => {
+      return {
+        data: false,
+        error: error,
+      };
+    });
+
+  return result;
+};
+
+export const opGetInverter = async (project_name) => {
+  let url = `${param_get_inverter}${project_name}`;
+  const result = await service.outProjectService
+    .get(url)
+    .then((res) => {
+      return {
+        data: res.data,
         error: false,
       };
     })
